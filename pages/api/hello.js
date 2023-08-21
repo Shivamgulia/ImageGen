a1136adb771b531f00b74082b87116cf17bf3bdd
Shivamgulia <92909697+Shivamgulia@users.noreply.github.com>
2023-08-21 22:58:12 +0530

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  async function query(data) {
+    const response = await fetch(
+      'https://api-inference.huggingface.co/models/minimaxir/sdxl-wrong-lora',
+      {
+        headers: {
+          Authorization: 'Bearer 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',
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.blob();
+    return result;
+  }
+  const response = await query({ inputs: req.body.text });
+  console.log(response);
+  await res.status(200).json({ image: response });
 }
